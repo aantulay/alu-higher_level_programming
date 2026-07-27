082302edc4a83b7f2ab56fd2df5e36e4c05eed64
aantulay <a.antulay@alustudent.com>
2026-07-27 21:08:52 +0200

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a MyList class based on the list type."""
+
+
+class MyList(list):
+    """A list that can also print its elements in sorted order."""
+
+    def print_sorted(self):
+        """Print the list in ascending sorted order.
+
+        The underlying list itself is left unmodified.
+        """
+        print(sorted(self))
+        
